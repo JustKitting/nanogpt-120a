41a73cfaffd787cd89536ddfb5d96861dfa4daf1
kit <kitherman@iusevimbtw.com>
2026-06-29 22:03:17 +0000

    Generate projection CTA accumulators

--- crates/cuda-kernels/src/utils/mma/projection_cta/accumulate.rs
+++ crates/cuda-kernels/src/utils/mma/projection_cta/accumulate.rs
@@ -11,103 +11,61 @@ use super::tile::{
     Nvfp4ProjectionCtaTile,
 };
 
-#[expect(clippy::too_many_arguments, reason = "CUDA ABI uses explicit buffers")]
-pub fn projection_accumulator(
-    input_bytes: &[u8],
-    input_scales: &[u8],
-    weight_bytes: &[u8],
-    weight_scales: &[u8],
-    tile: Nvfp4ProjectionCtaTile,
-    params: &Nvfp4ProjectionParams,
-    a_packs: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_A_PACKS>,
-    b_packs: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_B_PACKS>,
-    a_scales: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_A_SCALES>,
-    b_scales: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_B_SCALES>,
-) -> [f32; 4] {
-    let mut acc = [0.0_f32; 4];
-    let mut k_base = 0;
+macro_rules! projection_accumulator_fn {
+    ($name:ident, $stage_tiles:ident) => {
+        #[expect(clippy::too_many_arguments, reason = "CUDA ABI uses explicit buffers")]
+        pub fn $name(
+            input_bytes: &[u8],
+            input_scales: &[u8],
+            weight_bytes: &[u8],
+            weight_scales: &[u8],
+            tile: Nvfp4ProjectionCtaTile,
+            params: &Nvfp4ProjectionParams,
+            a_packs: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_A_PACKS>,
+            b_packs: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_B_PACKS>,
+            a_scales: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_A_SCALES>,
+            b_scales: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_B_SCALES>,
+        ) -> [f32; 4] {
+            let mut acc = [0.0_f32; 4];
+            let mut k_base = 0;
 
-    while k_base < params.input_dim {
-        stage_tiles(
-            input_bytes,
-            input_scales,
-            weight_bytes,
-            weight_scales,
-            tile,
-            k_base,
-            params,
-            a_packs,
-            b_packs,
-            a_scales,
-            b_scales,
-        );
-        thread::sync_threads();
-        let mut k_atom = 0;
-        while k_atom < NVFP4_PROJECTION_CTA_K_ATOMS {
-            mma_m16n8k64_scale4x_ue4m3(
-                load_a_fragments(a_packs, tile, k_atom),
-                load_b_fragments(b_packs, tile, k_atom),
-                &mut acc,
-                load_a_scale4(a_scales, tile, k_atom),
-                load_b_scale4(b_scales, tile, k_atom),
-            );
-            k_atom += 1;
+            while k_base < params.input_dim {
+                $stage_tiles(
+                    input_bytes,
+                    input_scales,
+                    weight_bytes,
+                    weight_scales,
+                    tile,
+                    k_base,
+                    params,
+                    a_packs,
+                    b_packs,
+                    a_scales,
+                    b_scales,
+                );
+                thread::sync_threads();
+                let mut k_atom = 0;
+                while k_atom < NVFP4_PROJECTION_CTA_K_ATOMS {
+                    mma_m16n8k64_scale4x_ue4m3(
+                        load_a_fragments(a_packs, tile, k_atom),
+                        load_b_fragments(b_packs, tile, k_atom),
+                        &mut acc,
+                        load_a_scale4(a_scales, tile, k_atom),
+                        load_b_scale4(b_scales, tile, k_atom),
+                    );
+                    k_atom += 1;
+                }
+                thread::sync_threads();
+                k_base += NVFP4_PROJECTION_CTA_K;
+            }
+
+            acc
         }
-        thread::sync_threads();
-        k_base += NVFP4_PROJECTION_CTA_K;
-    }
-
-    acc
+    };
 }
 
-#[expect(clippy::too_many_arguments, reason = "CUDA ABI uses explicit buffers")]
-pub fn projection_accumulator_aligned(
-    input_bytes: &[u8],
-    input_scales: &[u8],
-    weight_bytes: &[u8],
-    weight_scales: &[u8],
-    tile: Nvfp4ProjectionCtaTile,
-    params: &Nvfp4ProjectionParams,
-    a_packs: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_A_PACKS>,
-    b_packs: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_B_PACKS>,
-    a_scales: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_A_SCALES>,
-    b_scales: &mut SharedArray<u32, NVFP4_PROJECTION_CTA_B_SCALES>,
-) -> [f32; 4] {
-    let mut acc = [0.0_f32; 4];
-    let mut k_base = 0;
-
-    while k_base < params.input_dim {
-        stage_tiles_aligned(
-            input_bytes,
-            input_scales,
-            weight_bytes,
-            weight_scales,
-            tile,
-            k_base,
-            params,
-            a_packs,
-            b_packs,
-            a_scales,
-            b_scales,
-        );
-        thread::sync_threads();
-        let mut k_atom = 0;
-        while k_atom < NVFP4_PROJECTION_CTA_K_ATOMS {
-            mma_m16n8k64_scale4x_ue4m3(
-                load_a_fragments(a_packs, tile, k_atom),
-                load_b_fragments(b_packs, tile, k_atom),
-                &mut acc,
-                load_a_scale4(a_scales, tile, k_atom),
-                load_b_scale4(b_scales, tile, k_atom),
-            );
-            k_atom += 1;
-        }
-        thread::sync_threads();
-        k_base += NVFP4_PROJECTION_CTA_K;
-    }
-
-    acc
-}
+projection_accumulator_fn!(projection_accumulator, stage_tiles);
+projection_accumulator_fn!(projection_accumulator_aligned, stage_tiles_aligned);
 
 #[expect(clippy::too_many_arguments, reason = "CUDA ABI uses explicit buffers")]
 pub fn projection_accumulator_aligned_row_pair(
