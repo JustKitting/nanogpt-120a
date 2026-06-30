9447fa0216e7a3856bed0b08a3cac2f60c1b6c6a
kit <kitherman@iusevimbtw.com>
2026-06-30 08:16:09 +0000

    Share debug trace headers

--- src/training/debug_metrics/trace.rs
+++ src/training/debug_metrics/trace.rs
@@ -6,6 +6,9 @@ use super::super::diagnostics::{self, TensorUpdateDiagnostics};
 use super::super::launch::CudaTrainOutput;
 use crate::AppResult;
 
+const SUMMARY_HEADER: &str = "step\tsource\twindow_offset\tbatch_size\tseq_len\tupdate_count\tpositive_update_dot_count\tzero_grad_changed_count\tmax_update_to_weight_rms\tdlogits_rms\tdlogits_max\td_lm_head_rms\td_lm_head_max\td_embedding_rms\td_embedding_max\ttoken_embedding_global_before\ttoken_embedding_global_after\ttoken_embedding_changed_bytes\ttoken_embedding_hash_before\ttoken_embedding_hash_after";
+const TENSOR_HEADER: &str = "step\tsource\twindow_offset\tbatch_size\tseq_len\ttensor\tlen\tgrad_rms\tgrad_max\tgrad_nonzero\tgrad_finite\tweight_rms_before\tweight_rms_after\tdelta_rms\tdelta_max\tupdate_to_weight_rms\tdelta_grad_dot\tdelta_grad_cos\tpredicted_delta_rms\tpredicted_delta_grad_dot\tpredicted_delta_grad_cos\tquant_error_rms\tquant_error_to_predicted_delta_rms\tchanged_bytes\tchanged_scales\tglobal_before\tglobal_after";
+
 pub(in crate::training) struct DebugTraceLogger {
     summary: Option<BufWriter<File>>,
     tensors: Option<BufWriter<File>>,
@@ -23,14 +26,8 @@ impl DebugTraceLogger {
         fs::create_dir_all(&directory)?;
         let mut summary = BufWriter::new(File::create(directory.join("optimizer_summary.tsv"))?);
         let mut tensors = BufWriter::new(File::create(directory.join("optimizer_tensors.tsv"))?);
-        writeln!(
-            summary,
-            "step\tsource\twindow_offset\tbatch_size\tseq_len\tupdate_count\tpositive_update_dot_count\tzero_grad_changed_count\tmax_update_to_weight_rms\tdlogits_rms\tdlogits_max\td_lm_head_rms\td_lm_head_max\td_embedding_rms\td_embedding_max\ttoken_embedding_global_before\ttoken_embedding_global_after\ttoken_embedding_changed_bytes\ttoken_embedding_hash_before\ttoken_embedding_hash_after"
-        )?;
-        writeln!(
-            tensors,
-            "step\tsource\twindow_offset\tbatch_size\tseq_len\ttensor\tlen\tgrad_rms\tgrad_max\tgrad_nonzero\tgrad_finite\tweight_rms_before\tweight_rms_after\tdelta_rms\tdelta_max\tupdate_to_weight_rms\tdelta_grad_dot\tdelta_grad_cos\tpredicted_delta_rms\tpredicted_delta_grad_dot\tpredicted_delta_grad_cos\tquant_error_rms\tquant_error_to_predicted_delta_rms\tchanged_bytes\tchanged_scales\tglobal_before\tglobal_after"
-        )?;
+        writeln!(summary, "{SUMMARY_HEADER}")?;
+        writeln!(tensors, "{TENSOR_HEADER}")?;
         println!("debug_metrics_dir={}", directory.display());
         Ok(Self {
             summary: Some(summary),
