3558dc021ce048e41c871fdb560a42aa2dd703a4
kit <kitherman@iusevimbtw.com>
2026-06-30 20:21:04 +0000

    Share f16 CTA matmul body

--- crates/cuda-kernels/src/utils/f16_tc_matmul/cta.rs
+++ crates/cuda-kernels/src/utils/f16_tc_matmul/cta.rs
@@ -1,41 +1,7 @@
-use cuda_device::{DisjointSlice, SharedArray, thread};
-
-use super::cta_stage::{stage_tiles, stage_tiles_aligned};
-use super::cta_store::{store, store_aligned};
-use super::cta_sync::sync_before_next_k;
-use super::cta_tile::{CTA_A_ELEMS, CTA_B_ELEMS, CTA_K, CTA_M, CTA_N, active_tile};
-
-pub(super) fn cta_matmul_body(
-    a: &[u16],
-    b_t: &[u16],
-    mut out: DisjointSlice<f32>,
-    a_tile: &mut SharedArray<u16, CTA_A_ELEMS>,
-    b_tile: &mut SharedArray<u16, CTA_B_ELEMS>,
-    batch_count: u32,
-    m: u32,
-    n: u32,
-    k: u32,
-) {
-    let Some(tile) = active_tile(batch_count) else {
-        return;
-    };
-    cta_accumulators!(acc0, acc1, acc2, acc3);
-    let aligned = m % CTA_M == 0 && n % CTA_N == 0 && k % CTA_K == 0;
-    let mut k_base = 0;
-    while k_base < k {
-        if aligned {
-            stage_tiles_aligned(a, b_t, a_tile, b_tile, tile, m, n, k, k_base);
-        } else {
-            stage_tiles(a, b_t, a_tile, b_tile, tile, m, n, k, k_base);
-        }
-        thread::sync_threads();
-        cta_mma4!(a_tile, b_tile, tile, acc0, acc1, acc2, acc3);
-        sync_before_next_k(k_base, k);
-        k_base += CTA_K;
-    }
-    if aligned {
-        cta_store4!(store_aligned, tile, &mut out, m, n, acc0, acc1, acc2, acc3);
-    } else {
-        cta_store4!(store, tile, &mut out, m, n, acc0, acc1, acc2, acc3);
-    }
-}
+cta_bt_matmul_body_fn!(
+    cta_matmul_body,
+    u16,
+    u16,
+    super::cta_stage::stage_tiles,
+    super::cta_stage::stage_tiles_aligned
+);
