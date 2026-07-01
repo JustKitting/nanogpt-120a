1fdf4ba276aa7e7dd6ee2dedaa6964853f881416
kit <kitherman@iusevimbtw.com>
2026-07-01 08:36:07 +0000

    Compact GPT layer norm launchers

--- crates/cuda-kernels/src/gpt/layer_norm/launcher.rs
+++ crates/cuda-kernels/src/gpt/layer_norm/launcher.rs
@@ -17,37 +17,45 @@ pub struct LayerNormArgs<'a, 'out> {
     pub epsilon: f32,
 }
 
-pub struct GptLayerNormArgs<'a, 'out> {
-    pub stream: &'a CudaStream,
-    pub residual: &'a DeviceBuffer<f32>,
-    pub weight: Nvfp4DeviceTensor<'a>,
-    pub bias: Nvfp4DeviceTensor<'a>,
-    pub normalized: &'out mut DeviceBuffer<f32>,
-    pub normalized_amax: &'out mut DeviceBuffer<f32>,
-    pub mean: &'out mut DeviceBuffer<f32>,
-    pub inv_std: &'out mut DeviceBuffer<f32>,
-    pub row_count: u32,
-    pub embedding_dim: u32,
-    pub epsilon: f32,
+macro_rules! gpt_layer_norm_args {
+    ($name:ident $(, $extra:ident: $extra_ty:ty)*) => {
+        pub struct $name<'a, 'out> {
+            pub stream: &'a CudaStream,
+            pub residual: &'a DeviceBuffer<f32>,
+            pub weight: Nvfp4DeviceTensor<'a>,
+            pub bias: Nvfp4DeviceTensor<'a>,
+            pub normalized: &'out mut DeviceBuffer<f32>,
+            pub normalized_amax: &'out mut DeviceBuffer<f32>,
+            pub mean: &'out mut DeviceBuffer<f32>,
+            pub inv_std: &'out mut DeviceBuffer<f32>,
+            $(pub $extra: $extra_ty,)*
+            pub row_count: u32,
+            pub embedding_dim: u32,
+            pub epsilon: f32,
+        }
+    };
 }
 
-pub struct GptLayerNormSaveResidualF16Args<'a, 'out> {
-    pub stream: &'a CudaStream,
-    pub residual: &'a DeviceBuffer<f32>,
-    pub weight: Nvfp4DeviceTensor<'a>,
-    pub bias: Nvfp4DeviceTensor<'a>,
-    pub normalized: &'out mut DeviceBuffer<f32>,
-    pub normalized_amax: &'out mut DeviceBuffer<f32>,
-    pub mean: &'out mut DeviceBuffer<f32>,
-    pub inv_std: &'out mut DeviceBuffer<f32>,
-    pub residual_f16: &'out mut DeviceBuffer<u16>,
-    pub row_count: u32,
-    pub embedding_dim: u32,
-    pub epsilon: f32,
-}
+gpt_layer_norm_args!(GptLayerNormArgs);
+gpt_layer_norm_args!(GptLayerNormSaveResidualF16Args, residual_f16: &'out mut DeviceBuffer<u16>);
 
 pub struct LayerNormModule {
     module: kernels::LoadedModule,
+}
+
+macro_rules! gpt_layer_norm_launcher {
+    ($method:ident, $args:ty, $kernel:ident $(, $extra:ident)*) => {
+        pub fn $method(&self, args: $args) -> Result<(), DriverError> {
+            self.module.$kernel(
+                args.stream,
+                grid_x_config(args.row_count, GPT_LAYER_NORM_THREADS_PER_BLOCK),
+                args.residual, args.weight.bytes, args.weight.scales, args.bias.bytes,
+                args.bias.scales, args.weight.global_scale, args.bias.global_scale,
+                args.normalized, args.normalized_amax, args.mean, args.inv_std, $(args.$extra,)*
+                args.row_count, args.embedding_dim, args.epsilon,
+            )
+        }
+    };
 }
 
 impl LayerNormModule {
@@ -65,27 +73,6 @@ impl LayerNormModule {
         )
     }
 
-    pub fn gpt_layer_norm(&self, args: GptLayerNormArgs<'_, '_>) -> Result<(), DriverError> {
-        self.module.gpt_layer_norm_kernel(
-            args.stream,
-            grid_x_config(args.row_count, GPT_LAYER_NORM_THREADS_PER_BLOCK),
-            args.residual, args.weight.bytes, args.weight.scales, args.bias.bytes, args.bias.scales,
-            args.weight.global_scale, args.bias.global_scale, args.normalized, args.normalized_amax,
-            args.mean, args.inv_std, args.row_count, args.embedding_dim, args.epsilon,
-        )
-    }
-
-    pub fn gpt_layer_norm_save_residual_f16(
-        &self,
-        args: GptLayerNormSaveResidualF16Args<'_, '_>,
-    ) -> Result<(), DriverError> {
-        self.module.gpt_layer_norm_save_residual_f16_kernel(
-            args.stream,
-            grid_x_config(args.row_count, GPT_LAYER_NORM_THREADS_PER_BLOCK),
-            args.residual, args.weight.bytes, args.weight.scales, args.bias.bytes, args.bias.scales,
-            args.weight.global_scale, args.bias.global_scale, args.normalized, args.normalized_amax,
-            args.mean, args.inv_std, args.residual_f16, args.row_count, args.embedding_dim,
-            args.epsilon,
-        )
-    }
+    gpt_layer_norm_launcher!(gpt_layer_norm, GptLayerNormArgs<'_, '_>, gpt_layer_norm_kernel);
+    gpt_layer_norm_launcher!(gpt_layer_norm_save_residual_f16, GptLayerNormSaveResidualF16Args<'_, '_>, gpt_layer_norm_save_residual_f16_kernel, residual_f16);
 }
