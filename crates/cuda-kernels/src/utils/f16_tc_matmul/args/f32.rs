0efce2fd27b1cc3092f6b775c041a6d8c2c08492
kit <kitherman@iusevimbtw.com>
2026-07-01 08:25:22 +0000

    Compact F16 F32 matmul launchers

--- crates/cuda-kernels/src/utils/f16_tc_matmul/args/f32.rs
+++ crates/cuda-kernels/src/utils/f16_tc_matmul/args/f32.rs
@@ -1,56 +1,22 @@
 use cuda_core::{CudaStream, DeviceBuffer};
 
-pub struct F16TcMatmulF32Args<'a, 'out> {
-    pub stream: &'a CudaStream,
-    pub a: &'a DeviceBuffer<f32>,
-    pub b_t: &'a DeviceBuffer<f32>,
-    pub out: &'out mut DeviceBuffer<f32>,
-    pub batch_count: u32,
-    pub m: u32,
-    pub n: u32,
-    pub k: u32,
+macro_rules! f32_args {
+    ($name:ident, $rhs:ident: $rhs_ty:ty) => {
+        pub struct $name<'a, 'out> {
+            pub stream: &'a CudaStream,
+            pub a: &'a DeviceBuffer<f32>,
+            pub $rhs: &'a DeviceBuffer<$rhs_ty>,
+            pub out: &'out mut DeviceBuffer<f32>,
+            pub batch_count: u32,
+            pub m: u32,
+            pub n: u32,
+            pub k: u32,
+        }
+    };
 }
 
-pub struct F16TcMatmulF32RhsArgs<'a, 'out> {
-    pub stream: &'a CudaStream,
-    pub a: &'a DeviceBuffer<f32>,
-    pub rhs: &'a DeviceBuffer<f32>,
-    pub out: &'out mut DeviceBuffer<f32>,
-    pub batch_count: u32,
-    pub m: u32,
-    pub n: u32,
-    pub k: u32,
-}
-
-pub struct F16TcMatmulF32HalfRhsArgs<'a, 'out> {
-    pub stream: &'a CudaStream,
-    pub a: &'a DeviceBuffer<f32>,
-    pub rhs: &'a DeviceBuffer<u16>,
-    pub out: &'out mut DeviceBuffer<f32>,
-    pub batch_count: u32,
-    pub m: u32,
-    pub n: u32,
-    pub k: u32,
-}
-
-pub struct F16TcMatmulF32ATransposedRhsArgs<'a, 'out> {
-    pub stream: &'a CudaStream,
-    pub a: &'a DeviceBuffer<f32>,
-    pub rhs: &'a DeviceBuffer<f32>,
-    pub out: &'out mut DeviceBuffer<f32>,
-    pub batch_count: u32,
-    pub m: u32,
-    pub n: u32,
-    pub k: u32,
-}
-
-pub struct F16TcMatmulF32ATransposedHalfRhsArgs<'a, 'out> {
-    pub stream: &'a CudaStream,
-    pub a: &'a DeviceBuffer<f32>,
-    pub rhs: &'a DeviceBuffer<u16>,
-    pub out: &'out mut DeviceBuffer<f32>,
-    pub batch_count: u32,
-    pub m: u32,
-    pub n: u32,
-    pub k: u32,
-}
+f32_args!(F16TcMatmulF32Args, b_t: f32);
+f32_args!(F16TcMatmulF32RhsArgs, rhs: f32);
+f32_args!(F16TcMatmulF32HalfRhsArgs, rhs: u16);
+f32_args!(F16TcMatmulF32ATransposedRhsArgs, rhs: f32);
+f32_args!(F16TcMatmulF32ATransposedHalfRhsArgs, rhs: u16);
--- crates/cuda-kernels/src/utils/f16_tc_matmul/launcher_f32.rs
+++ crates/cuda-kernels/src/utils/f16_tc_matmul/launcher_f32.rs
@@ -6,110 +6,24 @@ use super::args::{
 };
 use super::launcher::{F16TcMatmulModule, cta_config, elements};
 
-impl F16TcMatmulModule {
-    pub fn batched_matmul_f32_input(
-        &self,
-        args: F16TcMatmulF32Args<'_, '_>,
-    ) -> Result<(), DriverError> {
-        assert!(args.a.len() >= elements(args.batch_count, args.m, args.k));
-        assert!(args.b_t.len() >= elements(args.batch_count, args.n, args.k));
-        assert!(args.out.len() >= elements(args.batch_count, args.m, args.n));
-
-        self.module.f16_cta_tc_matmul_f32_kernel(
-            args.stream,
-            cta_config(args.m, args.n, args.batch_count),
-            args.a,
-            args.b_t,
-            args.out,
-            args.batch_count,
-            args.m,
-            args.n,
-            args.k,
-        )
-    }
-
-    pub fn batched_matmul_f32_rhs(
-        &self,
-        args: F16TcMatmulF32RhsArgs<'_, '_>,
-    ) -> Result<(), DriverError> {
-        assert!(args.a.len() >= elements(args.batch_count, args.m, args.k));
-        assert!(args.rhs.len() >= elements(args.batch_count, args.k, args.n));
-        assert!(args.out.len() >= elements(args.batch_count, args.m, args.n));
-
-        self.module.f16_cta_tc_matmul_f32_rhs_kernel(
-            args.stream,
-            cta_config(args.m, args.n, args.batch_count),
-            args.a,
-            args.rhs,
-            args.out,
-            args.batch_count,
-            args.m,
-            args.n,
-            args.k,
-        )
-    }
-
-    pub fn batched_matmul_f32_half_rhs(
-        &self,
-        args: F16TcMatmulF32HalfRhsArgs<'_, '_>,
-    ) -> Result<(), DriverError> {
-        assert!(args.a.len() >= elements(args.batch_count, args.m, args.k));
-        assert!(args.rhs.len() >= elements(args.batch_count, args.k, args.n));
-        assert!(args.out.len() >= elements(args.batch_count, args.m, args.n));
-
-        self.module.f16_cta_tc_matmul_f32_half_rhs_kernel(
-            args.stream,
-            cta_config(args.m, args.n, args.batch_count),
-            args.a,
-            args.rhs,
-            args.out,
-            args.batch_count,
-            args.m,
-            args.n,
-            args.k,
-        )
-    }
-
-    pub fn batched_matmul_f32_a_transposed_rhs(
-        &self,
-        args: F16TcMatmulF32ATransposedRhsArgs<'_, '_>,
-    ) -> Result<(), DriverError> {
-        assert!(args.a.len() >= elements(args.batch_count, args.k, args.m));
-        assert!(args.rhs.len() >= elements(args.batch_count, args.k, args.n));
-        assert!(args.out.len() >= elements(args.batch_count, args.m, args.n));
-
-        self.module.f16_cta_tc_matmul_f32_a_transposed_rhs_kernel(
-            args.stream,
-            cta_config(args.m, args.n, args.batch_count),
-            args.a,
-            args.rhs,
-            args.out,
-            args.batch_count,
-            args.m,
-            args.n,
-            args.k,
-        )
-    }
-
-    pub fn batched_matmul_f32_a_transposed_half_rhs(
-        &self,
-        args: F16TcMatmulF32ATransposedHalfRhsArgs<'_, '_>,
-    ) -> Result<(), DriverError> {
-        assert!(args.a.len() >= elements(args.batch_count, args.k, args.m));
-        assert!(args.rhs.len() >= elements(args.batch_count, args.k, args.n));
-        assert!(args.out.len() >= elements(args.batch_count, args.m, args.n));
-
-        self.module
-            .f16_cta_tc_matmul_f32_a_transposed_half_rhs_kernel(
-                args.stream,
-                cta_config(args.m, args.n, args.batch_count),
-                args.a,
-                args.rhs,
-                args.out,
-                args.batch_count,
-                args.m,
-                args.n,
-                args.k,
+macro_rules! f32_matmul_launcher {
+    ($method:ident, $args:ty, $rhs:ident, $kernel:ident, a($a_rows:ident, $a_cols:ident), rhs($rhs_rows:ident, $rhs_cols:ident)) => {
+        pub fn $method(&self, args: $args) -> Result<(), DriverError> {
+            assert!(args.a.len() >= elements(args.batch_count, args.$a_rows, args.$a_cols));
+            assert!(args.$rhs.len() >= elements(args.batch_count, args.$rhs_rows, args.$rhs_cols));
+            assert!(args.out.len() >= elements(args.batch_count, args.m, args.n));
+            self.module.$kernel(
+                args.stream, cta_config(args.m, args.n, args.batch_count), args.a, args.$rhs,
+                args.out, args.batch_count, args.m, args.n, args.k,
             )
-    }
+        }
+    };
+}
+
+impl F16TcMatmulModule {
+    f32_matmul_launcher!(batched_matmul_f32_input, F16TcMatmulF32Args<'_, '_>, b_t, f16_cta_tc_matmul_f32_kernel, a(m, k), rhs(n, k));
+    f32_matmul_launcher!(batched_matmul_f32_rhs, F16TcMatmulF32RhsArgs<'_, '_>, rhs, f16_cta_tc_matmul_f32_rhs_kernel, a(m, k), rhs(k, n));
+    f32_matmul_launcher!(batched_matmul_f32_half_rhs, F16TcMatmulF32HalfRhsArgs<'_, '_>, rhs, f16_cta_tc_matmul_f32_half_rhs_kernel, a(m, k), rhs(k, n));
+    f32_matmul_launcher!(batched_matmul_f32_a_transposed_rhs, F16TcMatmulF32ATransposedRhsArgs<'_, '_>, rhs, f16_cta_tc_matmul_f32_a_transposed_rhs_kernel, a(k, m), rhs(k, n));
+    f32_matmul_launcher!(batched_matmul_f32_a_transposed_half_rhs, F16TcMatmulF32ATransposedHalfRhsArgs<'_, '_>, rhs, f16_cta_tc_matmul_f32_a_transposed_half_rhs_kernel, a(k, m), rhs(k, n));
 }
