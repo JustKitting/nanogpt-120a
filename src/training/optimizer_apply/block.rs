b61f843b653dd6d91b05738b23d74a62352f47b8
kit <kitherman@iusevimbtw.com>
2026-07-01 15:39:04 +0000

    Inline block Adam timing updates

--- src/training/optimizer_apply/block.rs
+++ src/training/optimizer_apply/block.rs
@@ -1,13 +1,13 @@
-use cuda_core::{CudaStream, DeviceBuffer, DriverError};
+use cuda_core::{CudaStream, DriverError};
 use rust_kernels_cuda::optimizer::OptimizerModule;
 
-use crate::upload::{UploadedBlock, UploadedModel, UploadedNvfp4};
+use crate::upload::{UploadedBlock, UploadedModel};
 
 use super::super::OptimizerTrace;
 use super::super::grad_block::BlockGradBuffers;
 use super::super::grads::BackwardBuffers;
 use super::super::optimizer::OptimizerScratch;
-use super::super::optimizer_state::{AdamState, BlockState, OptimizerStateBuffers};
+use super::super::optimizer_state::{BlockState, OptimizerStateBuffers};
 use super::adam::AdamUpdate;
 use super::layer_norm::update_layer_norm_timed;
 use super::timed_ms;
@@ -50,20 +50,10 @@ pub(super) fn update_block(
     trace: &mut OptimizerTrace,
 ) -> Result<(), DriverError> {
     trace.adam_ms += update_layer_norm_timed(adam, &mut block.ln_1, &grad.ln_1, &mut state.ln_1)?;
-    update_bias_timed(adam, trace, &mut block.attn_qkv.bias, &grad.d_attn_qkv_bias, &mut state.attn_qkv.bias)?;
-    update_bias_timed(adam, trace, &mut block.attn_c_proj.bias, &grad.d_attn_c_proj_bias, &mut state.attn_c_proj.bias)?;
+    trace.adam_ms += adam.update_timed(&mut block.attn_qkv.bias, &grad.d_attn_qkv_bias, &mut state.attn_qkv.bias)?;
+    trace.adam_ms += adam.update_timed(&mut block.attn_c_proj.bias, &grad.d_attn_c_proj_bias, &mut state.attn_c_proj.bias)?;
     trace.adam_ms += update_layer_norm_timed(adam, &mut block.ln_2, &grad.ln_2, &mut state.ln_2)?;
-    update_bias_timed(adam, trace, &mut block.mlp_up.bias, &grad.d_mlp_c_fc_bias, &mut state.mlp_up.bias)?;
-    update_bias_timed(adam, trace, &mut block.mlp_down.bias, &grad.d_mlp_c_proj_bias, &mut state.mlp_down.bias)
-}
-
-fn update_bias_timed(
-    adam: &mut AdamUpdate<'_, '_>,
-    trace: &mut OptimizerTrace,
-    tensor: &mut UploadedNvfp4,
-    grad: &DeviceBuffer<f32>,
-    state: &mut AdamState,
-) -> Result<(), DriverError> {
-    trace.adam_ms += adam.update_timed(tensor, grad, state)?;
+    trace.adam_ms += adam.update_timed(&mut block.mlp_up.bias, &grad.d_mlp_c_fc_bias, &mut state.mlp_up.bias)?;
+    trace.adam_ms += adam.update_timed(&mut block.mlp_down.bias, &grad.d_mlp_c_proj_bias, &mut state.mlp_down.bias)?;
     Ok(())
 }
