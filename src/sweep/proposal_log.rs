6cd77e41769405f4e25cd25601d9580ccc1d4553
kit <kitherman@iusevimbtw.com>
2026-07-01 17:18:14 +0000

    Group sweep proposal log writes

--- src/sweep/proposal_log.rs
+++ src/sweep/proposal_log.rs
@@ -7,17 +7,12 @@ mod format;
 use format::{ranked_tsv, selected_text, sources_tsv};
 
 pub fn write(sweep_dir: &Path, index: usize, proposal: &Proposal) -> SweepResult {
-    fs::write(
-        sweep_dir.join(format!("candidate_{index:04}_score.txt")),
-        selected_text(proposal),
-    )?;
-    fs::write(
-        sweep_dir.join(format!("candidate_{index:04}_ranked.tsv")),
-        ranked_tsv(proposal),
-    )?;
-    fs::write(
-        sweep_dir.join(format!("candidate_{index:04}_sources.tsv")),
-        sources_tsv(proposal),
-    )?;
+    for (suffix, text) in [
+        ("score.txt", selected_text(proposal)),
+        ("ranked.tsv", ranked_tsv(proposal)),
+        ("sources.tsv", sources_tsv(proposal)),
+    ] {
+        fs::write(sweep_dir.join(format!("candidate_{index:04}_{suffix}")), text)?;
+    }
     Ok(())
 }
